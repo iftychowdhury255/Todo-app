1a5cf2e0fec658e6c6c3ec2559e8ce1348501d95
iftychowdhury255 <iftychowdhury10212089@gmail.com>
2025-06-28 17:39:26 +0600

    edit and update button add in table

--- front_end/urls.py
+++ front_end/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('Complete/<int:pk>/', views.statusComplete, name='statusComplete'),
     
 ]
--- front_end/views.py
+++ front_end/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from back_end.models import CreateTodo
 
 # Create your views here.
@@ -8,3 +8,9 @@ def index(request):
         'all_todos': all_todos
     }
     return render(request, 'index.html', context)
+
+def statusComplete(request, pk):
+    is_Complete = CreateTodo.objects.get(pk=pk)
+    is_Complete.isComplete = not is_Complete.isComplete
+    is_Complete.save()
+    return redirect('home')
