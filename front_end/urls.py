371221558c260e4fe740b2674f80d5fd5d111b3f
iftychowdhury255 <iftychowdhury10212089@gmail.com>
2025-07-02 21:48:49 +0600

    delete frontend and backend complete

--- front_end/urls.py
+++ front_end/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('Complete/<int:pk>/', views.statusComplete, name='statusComplete'),
+    path('delete/<int:pk>/', views.deleteTodo, name='deleteTodo'),
     
 ]
--- front_end/views.py
+++ front_end/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from back_end.models import CreateTodo
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def index(request):
@@ -13,4 +14,10 @@ def statusComplete(request, pk):
     is_Complete = CreateTodo.objects.get(pk=pk)
     is_Complete.isComplete = not is_Complete.isComplete
     is_Complete.save()
+    return redirect('home')
+
+def deleteTodo(request, pk):
+    if request.method != 'POST':
+        todo = get_object_or_404(CreateTodo, pk=pk)
+        todo.delete()
     return redirect('home')
